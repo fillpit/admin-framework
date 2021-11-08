7c93f4488de1b74293523d02983abedc71b282b8
kenfei <kenfei@aliyun.com>
2021-11-08 14:14:31 +0800

    init

---
+++ src/main/java/com/kenfei/admin/core/config/CorsConfig.java
@@ -0,0 +1,40 @@
+package com.kenfei.admin.core.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * 跨域配置
+ * @author fei
+ * @date 2021/11/8 10:41
+ */
+@Configuration
+public class CorsConfig {
+  /**
+   * 添加跨域响应头
+   *
+   * @return 跨域响应头
+   */
+  @Bean
+  public CorsFilter corsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+    config.addAllowedOrigin("*");
+    config.addAllowedHeader("*");
+    config.addAllowedMethod(HttpMethod.GET);
+    config.addAllowedMethod(HttpMethod.POST);
+    config.addAllowedMethod(HttpMethod.PUT);
+    config.addAllowedMethod(HttpMethod.DELETE);
+    config.addAllowedMethod(HttpMethod.OPTIONS);
+    config.setMaxAge(3600L);
+    source.registerCorsConfiguration("/**", config);
+
+    return new CorsFilter(source);
+  }
+
+}
---
+++ src/test/java/com/admin/scancodeapi/ScanCodeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admin.scancodeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScanCodeApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
