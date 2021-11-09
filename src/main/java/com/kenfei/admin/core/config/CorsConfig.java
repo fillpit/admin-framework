d78bf5b9b9a12f7be4cd7ae51cb2fa8bbbd6b961
kenfei <kenfei@aliyun.com>
2021-11-09 14:47:56 +0800

    修复跨域配置不生效的问题

--- src/main/java/com/kenfei/admin/core/config/CorsConfig.java
+++ src/main/java/com/kenfei/admin/core/config/CorsConfig.java
@@ -6,35 +6,32 @@ import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * 跨域配置
+ *
  * @author fei
  * @date 2021/11/8 10:41
  */
+@EnableWebMvc
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
+
   /**
    * 添加跨域响应头
    *
    * @return 跨域响应头
    */
-  @Bean
-  public CorsFilter corsFilter() {
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    CorsConfiguration config = new CorsConfiguration();
-    config.setAllowCredentials(true);
-    config.addAllowedOrigin("*");
-    config.addAllowedHeader("*");
-    config.addAllowedMethod(HttpMethod.GET);
-    config.addAllowedMethod(HttpMethod.POST);
-    config.addAllowedMethod(HttpMethod.PUT);
-    config.addAllowedMethod(HttpMethod.DELETE);
-    config.addAllowedMethod(HttpMethod.OPTIONS);
-    config.setMaxAge(3600L);
-    source.registerCorsConfiguration("/**", config);
-
-    return new CorsFilter(source);
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedHeaders("*")
+        .allowedOrigins("*")
+        .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(), HttpMethod.DELETE.name(), HttpMethod.OPTIONS.name())
+        .maxAge(3600L);
   }
 
 }
